627d64f01e86b6e088e702aa8f800b7231349f46
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-15 12:03:38 -0400

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -4,8 +4,8 @@ const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button onClick={() => {props.removeFeature(props.feature)}} className="button">X</button>
+      {props.feature.name}(+ {props.feature.price})
     </li>
   );
 };
